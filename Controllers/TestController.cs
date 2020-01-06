82509bf80763275697e225e5a01873c477aa1506
Dibas Baral <dibas@transmissionmedia.ca>
2020-01-06 18:44:55 -0500

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BirdIsAWord.Data.Entities;
+using BirdIsAWord.Data;
+
+namespace BirdIsAWord.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController:ControllerBase
+    {
+        private readonly IRepositoryAsync<Sensor> _repository;
+
+        public TestController(IRepositoryAsync<Sensor> repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet("proxytable")]
+        public async Task<IActionResult> GetProximityTable()
+        {
+            var results = await _repository.ListAllAsync();
+            return Ok(results);
+        }
+        
+    }
+}
